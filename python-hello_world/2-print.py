03c7b7f4aef19c8bf79729655b47adb463692619
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-13 17:46:50 -0400

    Print a specific sentence

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-import sys
-sys.stdout.write('"Programming is like building a multilingual puzzle\n')
+words = ['Programming is like building a multilingual puzzle']
+print('"' + " ".join(words))
