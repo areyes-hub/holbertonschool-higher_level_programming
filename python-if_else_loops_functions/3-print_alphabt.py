a7168f8a202557131a93a7ba444ec75101776fec
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-14 15:40:54 -0400

    Prints alphabet without letters e and q

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if (letter == 101 or letter == 113):
-        continue
-    print(chr(letter), end="")
+for letter in range(ord('a'), ord('z') + 1):
+    if (letter != ord('e') and letter != ord('q')):
+        print("{}".format(chr(letter)), end="")
