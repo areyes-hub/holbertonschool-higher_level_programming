572e950a259b4bcd42d7ac5954076c7f366b9c07
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-15 16:41:20 -0400

    Make the functions portable

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-arg_len = len(sys.argv)
-count = 1
-if arg_len < 2:
-    print("{} arguments.".format(arg_len - 1))
-elif arg_len == 2:
-    print("{} argument.".format(arg_len - 1))
-    print("{}: {}".format(arg_len - 1, sys.argv[1]))
-else:
-    print("{} arguments:".format(arg_len - 1))
-    for arg in range(1, arg_len):
-        print("{}: {}".format(count, sys.argv[arg]))
-        count += 1
+    arg_len = len(sys.argv)
+    count = 1
+    if arg_len < 2:
+        print("{} arguments.".format(arg_len - 1))
+    elif arg_len == 2:
+        print("{} argument.".format(arg_len - 1))
+        print("{}: {}".format(arg_len - 1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(arg_len - 1))
+        for arg in range(1, arg_len):
+            print("{}: {}".format(count, sys.argv[arg]))
+            count += 1
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-addition = 0
-arg_len = len(sys.argv)
-for num in range(1, arg_len):
+    addition = 0
+    arg_len = len(sys.argv)
+    for num in range(1, arg_len):
         addition += int(sys.argv[num])
-print(addition)
+        print(addition)
