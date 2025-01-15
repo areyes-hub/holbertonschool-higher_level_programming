abb7d4ef29ca1c66548bf0f1dd227d1f905c76c2
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-15 18:01:00 -0400

    Import hidden_4 and print the files that do not start with __

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    for names in dir(hidden_4):
+        if names[:2] != '__':
+            print(names)
