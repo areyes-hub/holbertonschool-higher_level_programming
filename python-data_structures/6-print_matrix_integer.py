3cdaff32097ec7b623f57a30e80180066d76107e
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-20 11:12:29 -0400

    Refactor code

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for r in matrix:
-        for n in range(len(r)):
-            if n == len(r) - 1:
-                print("{}".format(r[n]), end="")
-            else:
-                print("{} ".format(r[n]), end="")
-        print()
+    for r in range(len(matrix)):
+        for n in range(len(matrix[r])):
+            print("{:d}".format(matrix[r][n]), end="")
+            if n != len(matrix[r]) - 1:
+                print(" ", end="")
+        print("")
