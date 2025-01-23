a1ea1f3c0fef231243709fb6ebe1025ea2e3eaa3
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-23 15:09:48 -0400

    Comply with pycodestyle

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -23,4 +23,3 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("say_my_name() missing 1 required \
                 positional argument: 'first_name'")
     print("My name is {} {}".format(first_name, last_name))
-
