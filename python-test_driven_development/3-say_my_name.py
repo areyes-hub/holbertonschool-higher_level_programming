e053b35a8990be04b649e462e877639d1688b2e8
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-23 14:46:16 -0400

    Comply with pycodestyle

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,8 +3,11 @@
 """
 Write a function that prints My name is <first name> <last name>
 Prototype: def say_my_name(first_name, last_name=""):
-first_name and last_name must be strings otherwise, raise a TypeError exception with the message first_name must be a string or last_name must be a string
+first_name and last_name must be strings otherwise, raise a
+TypeError exception with the message first_name must be a string or
+last_name must be a string
 """
+
 
 def say_my_name(first_name, last_name=""):
 
@@ -12,8 +15,8 @@ def say_my_name(first_name, last_name=""):
     Prints first name and last name
     """
 
-    if type(first_name) != str:
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if type(last_name) != str:
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
