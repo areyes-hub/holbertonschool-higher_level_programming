912f77a07a84a522031580d6d11dd01049798e21
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-23 11:47:32 -0400

    Print first and last name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 """
-3-say_my_name.py
+Write a function that prints My name is <first name> <last name>
+Prototype: def say_my_name(first_name, last_name=""):
+first_name and last_name must be strings otherwise, raise a TypeError exception with the message first_name must be a string or last_name must be a string
 """
 
 def say_my_name(first_name, last_name=""):
