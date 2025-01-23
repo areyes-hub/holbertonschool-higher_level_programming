3490b6728e393d42eb25fcac97bfa5ab566da920
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-23 15:08:03 -0400

    Comply with pycodestyle

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -20,5 +20,7 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     if not first_name:
-        raise TypeError("missing 1 required argument: first_name")
+        raise TypeError("say_my_name() missing 1 required \
+                positional argument: 'first_name'")
     print("My name is {} {}".format(first_name, last_name))
+
