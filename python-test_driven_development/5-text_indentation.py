62623272d0028e34bec1e988e35d5590a7000af7
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-23 11:45:11 -0400

    Add two new lines after every . ? : character

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+
+"""
+Write a function that prints a text with 2 new lines after each of these characters: ., ? and :
+Prototype: def text_indentation(text):
+text must be a string, otherwise raise a TypeError exception with the message text must be a string
+There should be no space at the beginning or at the end of each printed line
+"""
 def text_indentation(text):
+    """ Print two new lines after . : or ? """
     if type(text) != str:
         raise TypeError("text must be a string")
     i = 0
