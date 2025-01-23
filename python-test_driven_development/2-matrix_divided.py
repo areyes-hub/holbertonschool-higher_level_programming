f8e229169f44fa878954d4fefaf9d8224dbff105
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-23 14:35:22 -0400

    Update test file

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -27,4 +27,5 @@ def matrix_divided(matrix, div):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
             new_row.append(round(element / div, 2))
         new_matrix.append(new_row)
+        
     return new_matrix
