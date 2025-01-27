f46b33f698c0cc33d3e4b05b7df62a8eaeadfb5f
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-27 15:30:39 -0400

    Refactor code

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -14,21 +14,23 @@ class Square that defines a square by: (based on 4-square.py)
 Private instance attribute: size:
 property def size(self): to retrieve it
 property setter def size(self, value): to set it:
-size must be an integer, otherwise raise a TypeError exception with 
+size must be an integer, otherwise raise a TypeError exception with
 the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message 
+if size is less than 0, raise a ValueError exception with the message
 size must be >= 0
 Instantiation with optional size: def __init__(self, size=0):
 Public instance method: def area(self): that returns the current square area
-Public instance method: def my_print(self): that prints in stdout 
+Public instance method: def my_print(self): that prints in stdout
 the square with the character #:
 if size is equal to 0, print an empty line
     """
     def __init__(self, size=0):
         self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
@@ -37,8 +39,10 @@ if size is equal to 0, print an empty line
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     def area(self):
         return self.__size * self.__size
+
     def my_print(self):
         if self.__size == 0:
             print("")
