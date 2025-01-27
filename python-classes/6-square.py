6d868bcea4ff3bb08f8f16ad8d2319e80d87ecb9
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-27 15:35:47 -0400

    Refactor code

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -14,26 +14,34 @@ class Square that defines a square by: (based on 5-square.py)
 Private instance attribute: size:
 property def size(self): to retrieve it
 property setter def size(self, value): to set it:
-size must be an integer, otherwise raise a TypeError exception with 
+size must be an integer, otherwise raise a TypeError exception with
 the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message size must be >= 0
+if size is less than 0, raise a ValueError exception with the
+message size must be >= 0
 Private instance attribute: position:
 property def position(self): to retrieve it
 property setter def position(self, value): to set it:
-position must be a tuple of 2 positive integers, otherwise raise a TypeError exception 
+position must be a tuple of 2 positive integers, otherwise raise a
+TypeError exception
 with the message position must be a tuple of 2 positive integers
-Instantiation with optional size and optional position: def __init__(self, size=0, position=(0, 0)):
-Public instance method: def area(self): that returns the current square area
-Public instance method: def my_print(self): that prints in stdout the square with the character #:
+Instantiation with optional size and optional position:
+def __init__(self, size=0, position=(0, 0)):
+Public instance method: def area(self): that returns the current
+square area
+Public instance method: def my_print(self): that prints in stdout the
+square with the character #:
 if size is equal to 0, print an empty line
-position should be use by using space - Dont fill lines by spaces when position[1] > 0
+position should be use by using space - Dont fill lines by spaces when
+position[1] > 0
     """
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
@@ -42,9 +50,11 @@ position should be use by using space - Dont fill lines by spaces when position[
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     @property
     def position(self):
         return self.__position
+
     @position.setter
     def position(self, value):
         check = 0
@@ -62,8 +72,10 @@ position should be use by using space - Dont fill lines by spaces when position[
             self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
+
     def area(self):
         return self.__size * self.__size
+
     def my_print(self):
         if self.__size > 0:
             for y in range(self.__position[1]):
