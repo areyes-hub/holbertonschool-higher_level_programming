fff2d830e774177f0a4a4f193245fb8d2ba96ccc
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-27 14:25:59 -0400

    Implement getter and setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+This is the square module
+4-square.py
+This module contains the Square class
+"""
+
+
+class Square:
+    """
+class Square that defines a square by: (based on 3-square.py)
+
+Private instance attribute: size:
+property def size(self): to retrieve it
+property setter def size(self, value): to set it:
+size must be an integer, otherwise raise a TypeError exception 
+with the message size must be an integer
+if size is less than 0, raise a ValueError exception with the message 
+size must be >= 0
+Instantiation with optional size: def __init__(self, size=0):
+Public instance method: def area(self): that returns the current square area
+    """
+    def __init__(self, size=0):
+        self.size = size
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    def area(self):
+        return self.__size * self.__size
