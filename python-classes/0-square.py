b9402af1c3ca14907d1e3fca6eb31bd00aa38bea
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-27 13:29:30 -0400

    Create an empty class

---
+++ python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This is the square module
+0-square.py
+This module contains the Square class
+"""
+
+
+class Square:
+    """This is an empty class
+    defined without attributes
+    """
+    def __init__(self):
+        pass
