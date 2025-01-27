73dcad3ff01c21aa012b85d0e31b653bfa03c7a1
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-01-27 15:25:21 -0400

    Refactor code

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -13,11 +13,11 @@ class Square that defines a square by: (based on 2-square.py)
 
 Private instance attribute: size
 Instantiation with optional size: def __init__(self, size=0):
-size must be an integer, otherwise raise a TypeError exception with 
+size must be an integer, otherwise raise a TypeError exception with
 the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message 
+if size is less than 0, raise a ValueError exception with the message
 size must be >= 0
-Public instance method: def area(self): that returns the current 
+Public instance method: def area(self): that returns the current
 square area
 You are not allowed to import any module
     """
@@ -28,5 +28,6 @@ You are not allowed to import any module
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
         return self.__size * self.__size
