6e82d545fe94b22cadc02be00ccae11bd2037bf0
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-03 17:01:15 -0400

    Raise Exception with message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry (based on 5-base_geometry.py)
+"""
+
+
+class BaseGeometry:
+    """
+    Public instance method: def area(self):
+    Raises an Exception with the message area() is not implemented
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
