2dfb5bc41f303a2537710b56d9351ea2ccfce431
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-03 16:09:22 -0400

    Add test files and check class instance function

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function that returns True if the object is an instance of,
+or if the object is an instance of a class that inherited from,
+the specified class ; otherwise False
+Prototype: def is_kind_of_class(obj, a_class):
+"""
+def is_kind_of_class(obj, a_class):
+    """
+    Returns True if obj is instance of a_class, or is instance of
+    a class that inherited from. 
+    """
+    return isinstance(obj, a_class)
