ecc0114c24afd40fcd62e94c572ffc0088cff003
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-03 16:43:44 -0400

    Only sub class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Function that returns True if the object is an instance
+of a class that inherited (directly or indirectly) from the
+specified class ; otherwise False
+Prototype: def inherits_from(obj, a_class):
+"""
+def inherits_from(obj, a_class):
+    """
+    Returns True if obj is an instance of a class that inherited
+    directly or indirectly from a_class
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
