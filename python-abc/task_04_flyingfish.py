d5e574dce5a9dbae70d28b2d878ebd1a2ba020da
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-04 14:45:34 -0400

    Flying fish class

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+===================
+FlyingFish module
+===================
+"""
+
+
+class Fish:
+    """Fish class"""
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
+
+
+class Bird:
+    """Bird class"""
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """Inherits from both Fish and Bird classes"""
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
