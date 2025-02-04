12ee2271c2d27adc95041d809cc1be5bd44b221f
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-03 23:43:10 -0400

    Refactor code

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -18,13 +18,10 @@ message <name> must be an integer
 if value is less or equal to 0: raise a ValueError exception with
 the message <name> must be greater than 0
     """
-
     def area(self):
-        """Raises an Exception with the message area() is not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Public instance method that validates value"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
