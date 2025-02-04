bea3e355d3e077c435facf978cde9e52c42bfbac
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-03 23:46:23 -0400

    Refactor code

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -15,12 +15,9 @@ size must be a positive integer, validated by integer_validator
 the area() method must be implemented
     """
     def __init__(self, size):
-        """validate size input"""
         super().__init__(size, size)
         self.integer_validator("size", size)
-        """Assign the values"""
         self.__size = size
 
-    """Calculate the area"""
     def area(self):
         return self.__size ** 2
