2968474a44cbc8d32361b8c7ce566a3b7abc7396
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-03 20:30:06 -0400

    Refactor code

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,14 +7,14 @@ class BaseGeometry (based on 6-base_geometry.py)
 class BaseGeometry:
     """
     Public instance method: def area(self):
-    Raises an Exception with the message area() is not implemented
     """
     def area(self):
+        """Raises an Exception with the message area() is not implemented"""
         raise Exception("area() is not implemented")
 
-    """Public instance method that validates value"""
     def integer_validator(self, name, value):
+        """Public instance method that validates value"""
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
