f76fd33881634ad2231881e230bda004c20e90f3
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-03 23:44:11 -0400

    Refactor code

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -14,9 +14,7 @@ Instantiation with width and height: def __init__(self, width, height):
 width and height must be private. No getter or setter
 width and height must be positive integers, validated by integer_validator
     """
-
     def __init__(self, width, height):
-        """validate width and height input"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
