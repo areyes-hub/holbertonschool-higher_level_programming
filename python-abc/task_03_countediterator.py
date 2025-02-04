1877c0557da0a1d86febdf5c55a39f71e8b6cdef
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-04 14:21:10 -0400

    Counted iterator

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+=======================
+CountedIterator module
+=======================
+"""
+
+
+class CountedIterator:
+    """Extends iter cass"""
+    def __init__(self, some_iterable):
+        self.iterator = iter(some_iterable)
+        self.counter = 0
+
+    def get_count(self):
+        return self.counter
+
+    def __next__(self):
+        self.counter += 1
+        if not next(self.iterator):
+            raise StopIteration
+        return self.counter
