e6e51a829475ae56797520000e9c37a15e275fb7
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-04 09:54:26 -0400

    Create abstract animal class

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from abc import ABC, abstractmethod
+"""
+====================
+ABC module
+====================
+"""
+
+
+class Animal(ABC):
+    """Abstract class"""
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    """Inherits from class Animal and implements sound method"""
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    """Inherits from class Animal and implements sound method"""
+    def sound(self):
+        return "Meow"
