9a61e99afcdff53fa566f7f1522c0c101b432dcd
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-03 21:17:11 -0400

    Refactor code

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 """
-class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+module with class Rectangle
 """
 
 
@@ -11,10 +12,10 @@ Instantiation with width and height: def __init__(self, width, height):
 width and height must be private. No getter or setter
 width and height must be positive integers, validated by integer_validator
     """
+
     def __init__(self, width, height):
         """validate width and height input"""
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        """Assign the values"""
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
