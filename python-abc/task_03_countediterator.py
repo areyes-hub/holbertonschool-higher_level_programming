2be71f6aba12381771b297460cb1f8551b0b20e2
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-04 21:00:45 -0400

    Refactor code

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -6,7 +6,7 @@ CountedIterator module
 """
 
 
-class CountedIterator:
+class CountedIterator(object):
     """Extends iter cass"""
     def __init__(self, some_iterable):
         self.iterator = iter(some_iterable)
