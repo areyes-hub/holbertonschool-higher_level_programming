e28ef478d7ecbde2b747df2f01f77a5f4b87a549
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-05 18:14:50 -0400

    Refactor code

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -17,8 +17,8 @@ print() should print, and str() should return, the square
 description: [Square] <width>/<height>
     """
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
