793329e1a2c9f29760c840a364d0b3835d89c329
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-10 18:06:59 -0400

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Student class module
+"""
+
+
+class Student:
+    """
+    Defines a Student class with public instance
+    attributes: first_name, last_name, age and a
+    public instance method: to_json(self, attrs=None)
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs:
+            new_dict = dict()
+            for i in range(len(attrs)):
+                if attrs[i] not in self.__dict__:
+                    continue
+                new_dict[attrs[i]] = self.__dict__[attrs[i]]
+            return new_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        self.__dict__ = json
