ae2290b1be20d7e08c81005dc6c7fa7a9d8e4bd9
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-10 18:10:25 -0400

    Student to disk and reload

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -11,7 +11,7 @@ class Student:
     attributes: first_name, last_name, age and a
     public instance method: to_json(self, attrs=None)
     """
-    
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
