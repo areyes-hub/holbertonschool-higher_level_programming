d4161dc1fde6375cd575038a324f4ef823270d59
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-10 11:16:15 -0400

    Write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,10 +8,5 @@ def write_file(filename="", text=""):
     """write_file writes a given text string to a file"""
     with open(filename, "w", encoding="utf-8") as file:
         file.write(text)
-    
-    with open(filename, "r", encoding="utf-8") as file:
-        count = 0
-        for words in file.readlines():
-            for _ in words:
-                count += 1
-    return count
+
+    return len(text)
