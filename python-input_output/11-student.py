181b6a4d186365dbd584e37078018ef90d1c1b6c
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-12 15:29:34 -0400

    Refactor code

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -12,7 +12,7 @@ class Student:
     public instance method: to_json(self, attrs=None)
     """
 
-    def __init__(self, first_name="J", last_name="S", age=1):
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -20,16 +20,13 @@ class Student:
     def to_json(self, attrs=None):
         if attrs:
             new_dict = dict()
-            for i in attrs:
-                if i in self.__dict__:
-                    new_dict[i] = self.__dict__[i]
+            for i in range(len(attrs)):
+                if attrs[i] not in self.__dict__:
+                    continue
+                new_dict[attrs[i]] = self.__dict__[attrs[i]]
             return new_dict
         else:
-            return {
-                "last_name": self.last_name,
-                "first_name": self.first_name,
-                "age": self.age
-            }
+            return self.__dict__
 
     def reload_from_json(self, json):
         self.__dict__ = json
