2fdbbd11041c509ca3603c59b019916696685948
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-17 20:34:30 -0400

    Server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -10,27 +10,27 @@ import json
 class Server(http.server.BaseHTTPRequestHandler):
     """simple server"""
     def do_GET(self):
-        if  self.path.endswith("/data"):
+        if  self.path == "/data":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(bytes(json.dumps(data), "utf-8"))
-        elif self.path.endswith("/info"):
+        elif self.path == "/info":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             info = {"version": "1.0", "description": "A simple \
 API built with http.server"}
             self.wfile.write(bytes(json.dumps(info), "utf-8"))
-        # elif self.path.endswith("/"):
-        #     self.send_response(200)
-        #     self.send_header("Content-type", "text/html")
-        #     self.end_headers()
-        #     self.wfile.write(bytes(
-        #         "<html><head><title></title><body><p>Hello, this is a \
-        #             simple API!</p></body></head></html>", "utf-8"
-        #         ))
+        elif self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes(
+                "<html><head><title></title><body><p>Hello, this is a \
+                    simple API!</p></body></head></html>", "utf-8"
+                ))
         else:
             self.send_response(404, "Not Found")
             self.send_header("Content-type", "text/html")
