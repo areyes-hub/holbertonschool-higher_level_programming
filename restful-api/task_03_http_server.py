cf460a2e6dbe27d2dae6426dde37040a015c9330
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-02-17 20:53:32 -0400

    Server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -15,30 +15,30 @@ class Server(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-type", "application/json")
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(bytes(json.dumps(data), "utf-8"))
+            self.wfile.write(json.dumps(data).encode("utf-8"))
         elif self.path == "/info":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             info = {"version": "1.0", "description": "A simple \
 API built with http.server"}
-            self.wfile.write(bytes(json.dumps(info), "utf-8"))
+            self.wfile.write(json.dumps(info).encode("utf-8"))
         elif self.path == "/":
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
-            self.wfile.write(bytes(
-                "<html><head><title></title><body><p>Hello, this is a \
-                    simple API!</p></body></head></html>", "utf-8"
-                ))
+            self.wfile.write(
+                "<html><body><p>Hello, this is a \
+                    simple API!</p></body></html>".encode("utf-8")
+                )
         else:
             self.send_response(404, "Not Found")
             self.send_header("Content-type", "text/html")
             self.end_headers()
-            self.wfile.write(bytes(
-                "<html><body><h1>Endpoint not found</h1></body></html>", \
-                    "utf-8"
-                ))
+            self.wfile.write(
+                "<html><body><p>Endpoint not found</p></body></html>". \
+                    encode("utf-8")
+                )
 
 def run_server(port):
     with socketserver.TCPServer(("", port), Server) as httpd:
