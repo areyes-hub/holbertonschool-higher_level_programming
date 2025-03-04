bbd93904d8eee72292924f40dfb7f4496a0015a1
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-04 17:10:03 -0400

    Refactor code

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,15 +5,10 @@ USE hbtn_0d_usa;
 CREATE TABLE
 IF NOT EXISTS cities
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    state_id INT NOT NULL,
+    id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+    state_id INT NOT NULL FOREIGN KEY,
     name VARCHAR
 (256) NOT NULL,
-    PRIMARY KEY
-(id),
-    UNIQUE
-(id),
-    FOREIGN KEY
-(state_id) REFERENCES states
+    REFERENCES states
 (id)
 );
