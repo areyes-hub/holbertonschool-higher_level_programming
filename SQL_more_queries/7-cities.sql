0a7da1a430d4d1b993c7ecc72424b5ac171512f5
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-04 17:06:46 -0400

    Refactor code

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -9,11 +9,11 @@ IF NOT EXISTS cities
     state_id INT NOT NULL,
     name VARCHAR
 (256) NOT NULL,
-    UNIQUE
-(id),
     PRIMARY KEY
 (id),
+    UNIQUE
+(id),
     FOREIGN KEY
-(state_id) REFERENCES States
+(state_id) REFERENCES states
 (id)
 );
