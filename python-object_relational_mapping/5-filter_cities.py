15c34e18c61773044319f1966870f1d6207ca9e2
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-06 15:13:53 -0400

    Refactor code

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-takes in the name of a state as an argument and lists 
+takes in the name of a state as an argument and lists
 all cities of that state, using the database hbtn_0e_4_usa
 """
 import MySQLdb
@@ -28,8 +28,7 @@ def main():
     cur.execute("SELECT cities.name\
                 FROM cities\
                 LEFT JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s ORDER BY cities.id ASC", (state_name,)
-        )
+                WHERE states.name = %s ORDER BY cities.id ASC", (state_name,))
     query_rows = cur.fetchall()
     count = 0
     for row in query_rows:
