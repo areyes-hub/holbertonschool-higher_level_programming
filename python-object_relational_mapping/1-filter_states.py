ad78fb3307c82fec078bfa88148a246bcfbb256a
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-06 10:08:32 -0400

    Refactor code

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,9 +10,6 @@ def main():
     """
     main function that runs the module
     """
-    if len(sys.argv) < 4:
-        return
-
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     mysql_database = sys.argv[3]
