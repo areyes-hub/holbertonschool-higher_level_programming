84e7e53ce7f8736938c69868ab69a661ac06ae57
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-06 10:17:29 -0400

    Filter by user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module lists state info based on user input
+"""
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    main function that runs the module"""
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_database,
+        charset="utf8"
+        )
+    cur = conn.cursor()
+    cur.execute(
+        f"SELECT * FROM states WHERE name = '{state_name}' ORDER BY id ASC"
+        )
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
