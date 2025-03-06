2b8a7e8ad454eee917a3e824c2c3f63469baa863
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-06 09:28:41 -0400

    Filter states by first letter

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module filters state names by thir first letter
+"""
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    main function that runs the module
+    """
+    if len(sys.argv) < 4:
+        return
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_database = sys.argv[3]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_database,
+        charset="utf8"
+        )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
