8051f4fdf5517868e207608f6afecb25da89457b
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-06 19:10:17 -0400

    Refactor code

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-lists all State objects that have the letter 'a' from the database hbtn_0e_6_usa
+lists all State objects that have
+the letter 'a' from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
