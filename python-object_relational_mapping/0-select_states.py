9cd0a88b67537e5f3f18f09b721ecfb566dd642c
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-06 08:29:42 -0400

    Refactor code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,21 +5,22 @@ This module lists all states of the state table
 import MySQLdb
 
 
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    passwd="C0d3r76!!!",
-    db="hbtn_0e_0_usa",
-    charset="utf8"
-    )
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+def main():
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        passwd="C0d3r76!!!",
+        db="hbtn_0e_0_usa",
+        charset="utf8"
+        )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
 
 if __name__ == "__main__":
-    conn.run()
+    main()
