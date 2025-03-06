70886582e41a55f2df597920f8eaed52885dc56f
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-06 09:08:38 -0400

    Refactor code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,18 +3,26 @@
 This module lists all states of the state table
 """
 import MySQLdb
+import sys
 
 
 def main():
     """
     main function that runs the module
     """
+    if len(sys.argv) < 4:
+        return
+    
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_database = sys.argv[3]
+
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user="root",
-        passwd="C0d3r76!!!",
-        db="hbtn_0e_0_usa",
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_database,
         charset="utf8"
         )
     cur = conn.cursor()
