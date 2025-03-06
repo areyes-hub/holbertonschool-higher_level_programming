29060460cdbce828623f63de25515a05d4611bba
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-06 09:30:33 -0400

    Refactor code

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -26,7 +26,7 @@ def main():
         charset="utf8"
         )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
