82d758d44bfe86e2cf0a29302e58114d9863fa88
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-07 09:59:23 -0400

    Refactor code

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -22,10 +22,9 @@ def main():
         db=mysql_database,
         charset="utf8"
         )
-    letter_n = "n".upper()
     cur = conn.cursor()
     cur.execute(f"SELECT * FROM states WHERE states.name\
-                LIKE '{letter_n}%' ORDER BY states.id ASC")
+                LIKE 'N%' ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print("({}, '{}')".format(row[0], row[1]))
