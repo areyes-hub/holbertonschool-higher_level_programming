b470fd68fcd41300c27f6f160b31aef1370c8e23
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-07 09:34:32 -0400

    Refactor code

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,7 +6,7 @@ the letter 'a' from the database hbtn_0e_6_usa
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
@@ -15,8 +15,9 @@ if __name__ == "__main__":
             sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     for state in session.query(State).order_by(State.id).all():
-        if 'a' in state.name:
+        if "a" in state.name or "A" in state.name:
             print("{}: {}".format(state.id, state.name))
     session.close()
