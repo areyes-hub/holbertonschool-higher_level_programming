2690278164b35ee245f643dadab73877c8b29f62
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-07 09:37:17 -0400

    Refactor code

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for state in session.query(State).order_by(State.id).all():
-        if "a" in state.name or "A" in state.name:
+        if "a" in state.name:
             print("{}: {}".format(state.id, state.name))
     session.close()
